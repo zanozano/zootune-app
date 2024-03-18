41228725026cc739798b97137266b07c324e00e3
Cristobal Andres Manzano Martinez <zano@live.cl>
2024-03-18 22:25:13 +0700

    update

--- public/assets/js/Consulta.js
+++ public/assets/js/Consulta.js
@@ -1,29 +1,39 @@
-const animals = [
-	{
-		name: "Lion",
-		imagen: "../assets/imgs/Leon.webp",
-		sonido: "../assets/sounds/Rugido.mp3"
-	},
-	{
-		name: "Wolf",
-		imagen: "../assets/imgs/Lobo.webp",
-		sonido: "../assets/sounds/Aullido.mp3"
-	},
-	{
-		name: "Bear",
-		imagen: "../assets/imgs/Oso.webp",
-		sonido: "../assets/sounds/Gruñido.mp3"
-	},
-	{
-		name: "Snake",
-		imagen: "../assets/imgs/Serpiente.webp",
-		sonido: "../assets/sounds/Siseo.mp3"
-	},
-	{
-		name: "Eagle",
-		imagen: "../assets/imgs/Aguila.webp",
-		sonido: "../assets/sounds/Chillido.mp3"
+const animals = {
+	getData: async () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const animalData = [
+					{
+						name: "Lion",
+						imagen: "../assets/imgs/Leon.webp",
+						sonido: "../assets/sounds/Rugido.mp3"
+					},
+					{
+						name: "Wolf",
+						imagen: "../assets/imgs/Lobo.webp",
+						sonido: "../assets/sounds/Aullido.mp3"
+					},
+					{
+						name: "Bear",
+						imagen: "../assets/imgs/Oso.webp",
+						sonido: "../assets/sounds/Gruñido.mp3"
+					},
+					{
+						name: "Snake",
+						imagen: "../assets/imgs/Serpiente.webp",
+						sonido: "../assets/sounds/Siseo.mp3"
+					},
+					{
+						name: "Eagle",
+						imagen: "../assets/imgs/Aguila.webp",
+						sonido: "../assets/sounds/Chillido.mp3"
+					}
+				];
+
+				resolve(animalData);
+			}, 1000);
+		});
 	}
-];
+};
 
 export default animals;
